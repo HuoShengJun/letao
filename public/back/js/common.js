e0c9f52a301cccf4baa24ce513120cb14eb7a760
huoshengjun <hsj@ds.com>
2018-04-07 19:34:35 +0800

    首页完成

--- public/back/js/common.js
+++ public/back/js/common.js
@@ -11,7 +11,28 @@ $(document).ajaxStop(function(){
   setTimeout(function(){
     NProgress.done();
   },500);
-})
+});
+
+
+//在一进入页面进行登录状态获取
+//如果后端响应头中设置了 Content-Type:application/json
+//jquery 会自动识别，将返回数据类型，当成json字符串解析对象
+if(location.href.indexOf("login.html")===-1){
+  console.log("hehehe");
+  $.ajax({
+    type:"get",
+    url:"/employee/checkRootLogin",
+    success:function(info){
+      console.log(info);
+      if(info.success){
+        console.log("登录了")
+      }
+      if(info.error===400){
+        location.href ="login.html";
+      }
+    }
+  })
+}
 
 $(function(){
   //1-二级分类切换功能
